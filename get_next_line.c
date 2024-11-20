d90b837d74959def89443a81fb476d523ed16eb7
Brahim Dani <brdani@3E5.42angouleme.fr>
2024-11-20 10:38:55 +0100

    UpdateRead_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: brdani <brdani@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 04:45:02 by brdani            #+#    #+#             */
-/*   Updated: 2024/11/16 06:55:20 by brdani           ###   ########.fr       */
+/*   Updated: 2024/11/20 10:37:01 by brdani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,28 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
 	stash = read_line(fd, stash);
+	if (!stash)
+		return (NULL);
 	// extract depuis le buffer sur une ligne
 	// tout nettoyer et free 
 	return (line);
 }
+
+char	*ft_join_free(char *stash, char *buffer)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(stash, buffer);
+	free(stash);
+	return (tmp);
+}
+
 char	*read_line(int fd, char *stash)
 {
 	char	*buffer;
 	int		bt;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buffer == NULL);
 		return (NULL);
 	bt = 1;
@@ -38,18 +50,16 @@ char	*read_line(int fd, char *stash)
 		bt = read(fd, buffer, BUFFER_SIZE);
 		if (bt == -1)
 		{
-			free (buffer);
 			free (stash);
-			return (NULL);
+			free (buffer);
+			return (stash);
 		}
-		buffer[bt] = 0;
-		//Concatener la stash et le buffer et la libere (strjoin ?)
-		stash = ft_strjoin(stash, buffer);
-		free (stash);
-		//Trouver le /n (strchr ?) et stopper la boucle car lue
+		buffer[bt] = '\0';
+		stash = ft_join_free(stash, buffer);
 		if (ft_strchr(stash, '\n'));
-			bt--;       
+			break;       
 	}
 	free (buffer);
 	return (stash);
 }
+
